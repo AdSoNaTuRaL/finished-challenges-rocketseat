783ea95bf135f5cbac68bf877c7d495aa391b116
adsonatural <adsonhenriquesilva@gmail.com>
2020-09-08 16:48:07 +0200

    First commit - finished challenges

---
+++ 03 - concepts-reactjs/src/App.js
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from "react";
+
+import "./styles.css";
+import api from "./services/api";
+
+function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data);
+    })
+  }, []);
+
+  async function handleAddRepository() {
+    const response = await api.post('repositories',
+    {
+      url: "https://github.com/AdSoNaTuRaL",
+      title: "Adson Henrique",
+      techs: ["Node", "React", "TypeScript"]
+    });
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
+  }
+
+  async function handleRemoveRepository(id) {
+    setRepositories(repositories.filter(repository => repository.id !== id))
+  }
+
+  return (
+    <div>
+      <ul data-testid="repository-list">
+        {repositories.map(repository => 
+        <li key={repository.id}>
+          {repository.title}
+
+          <button onClick={() => handleRemoveRepository(repository.id)}>
+            Remover
+          </button>
+
+        </li>)}
+      </ul>
+
+      <button onClick={handleAddRepository}>Adicionar</button>
+    </div>
+  );
+}
+
+export default App;
